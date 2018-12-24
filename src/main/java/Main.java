4d864ca502e559f0a2dd1f79cc249e5b32b6ac59
Adelaiten <karoltrzaska19@gmail.com>
2018-12-24 15:46:50 +0100

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,21 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+import java.sql.Connection;
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        // create a server on port 8000
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        Connector connector = new Connector();
+        Connection connection = connector.connect("karol", "4313284");
+
+        // set routes
+        server.createContext("/form", new Form(connection));
+
+        server.setExecutor(null); // creates a default executor
+
+        // start listening
+        server.start();
+    }
 }
