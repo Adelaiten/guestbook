f824753eb93303e8b062dbdb03977847145c0e8a
Adelaiten <karoltrzaska19@gmail.com>
2018-12-24 15:45:58 +0100

    connector sql

---
+++ src/main/java/Connector.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connector {
+
+    public Connection connect(String dbUser, String dbPassword) {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/guestbook", dbUser, dbPassword);
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        return c;
+    }
+}
